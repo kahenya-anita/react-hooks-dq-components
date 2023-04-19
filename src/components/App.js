7d400ff88a8f035562eb8801f4b84cfed7a9d577
kahenya-anita <anitakahenya1@gmail.com>
2023-04-19 12:52:47 +0300

    Components hierachy

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import MainContent from "./MainContent";
+import TopMenu from "./TopMenu";
+import SideMenu from "./SideMenu";
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return (
+    <div className="app-container">
+      <TopMenu />
+      <SideMenu />
+      <MainContent />
+    </div>
+  );
 }
 
 export default App;
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,7 +1,14 @@
 import React from "react";
+import GenrePicker from "./GenrePicker";
+import SideMenu from "./SideMenu";
 
 function MainContent() {
-  return <main>{/* What components go here? */}</main>;
+  return (
+    <main>
+      <GenrePicker />
+      <AlbumList />
+    </main>
+  );
 }
 
 export default MainContent;
